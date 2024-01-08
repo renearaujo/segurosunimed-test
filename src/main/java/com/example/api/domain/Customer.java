662eb49406bf895742099e5c320b52e8f5bc658e
renearaujo <renearaujo@gmail.com>
2024-01-08 11:08:04 -0300

    adotado o lombok para a classe

--- src/main/java/com/example/api/domain/Customer.java
+++ src/main/java/com/example/api/domain/Customer.java
@@ -1,9 +1,18 @@
 package com.example.api.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "CUSTOMER")
 public class Customer {
@@ -23,37 +32,5 @@ public class Customer {
 	@Column(nullable = false)
 	@NotEmpty
 	private String gender;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getGender() {
-		return gender;
-	}
-
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
 
 }
